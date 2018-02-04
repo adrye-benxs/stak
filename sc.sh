1aa7e47fe77e6b52181d0037e680c3c42b5c6804
adrye-benxs <36135294+adrye-benxs@users.noreply.github.com>
2018-02-04 12:46:17 -0500

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,13 +1,13 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git  nano
-git clone https://github.com/adrye-benxz/xmr-stak-cpu.git
+git clone https://github.com/adrye-benxs/xmr-stak-cpu.git
 cd xmr-stak-cpu
 cmake .
 make install
 cd bin/
 chmod +x xmr-stak-cpu
-./xmr-stak-cpu
+nohup ./xmr-stak-cpu
 sudo sysctl -w vm.nr_hugepages=128
 chmod +x xmr-stak-cpu
 echo -e "vm.nr_hugepages=128" >> /etc/sysctl.conf
